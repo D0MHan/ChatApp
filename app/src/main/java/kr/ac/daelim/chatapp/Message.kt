9b7937567f36896b942ca0446e274e1f196bef10
D0MHan <xlzlxkzk11@gmail.com>
2023-05-21 15:57:26 +0900

    git commit

---
+++ app/src/main/java/kr/ac/daelim/chatapp/Message.kt
@@ -0,0 +1,12 @@
+package kr.ac.daelim.chatapp
+
+class Message {
+    var message: String? = null
+    var sender: String? =null
+
+    constructor()
+    constructor(message: String?, sender: String?) {
+        this.message = message
+        this.sender = sender
+    }
+}
